07d8cfa6a1b4bc67ad2a3071cae712c5efac350b
sketodee <shobandejames@gmail.com>
2022-08-01 13:15:40 +0100

    fixed decimal points for exchange rate

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -12,12 +12,14 @@ const Converter = () => {
         const res = await fetch('https://api.exchangerate.host/symbols')
         const data = await res.json()
         setCurrency(data.symbols)
+        // console.log(data)
     }
 
     const convertCurrency = async () => {
         const res = await fetch(`https://api.exchangerate.host/convert?from=${from}&to=${to}`)
         const data = await res.json()
-        setExchange(data.result)
+        setExchange(data.result.toFixed(2))
+        console.log(exchange)
     }
 
     useEffect(() => {
@@ -27,21 +29,23 @@ const Converter = () => {
     const result = Object.keys(currency).map(function (key) {
         return { name: currency[key].code, desc: currency[key].description };
     });
+
+
     return (
-        <div className='bg-dark text-white pb-5 p-3'>
+        <div className='bg-dark text-white pb-5 p-3' style={{height: "60vh"}}>
             <div className='d-flex justify-content-center py-3'>
                 <h1 className='display-1'>{exchange * amount} {to}</h1>
             </div>
 
             <div className='d-flex justify-content-between container'>
 
-                <div className=''>
+                <div className='pe-3'>
                     <p>Amount</p>
                     <input placeholder='enter amount' className='form-control form-control-lg bg-dark text-white border-warning' type="text" value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
                 </div>
 
 
-                <div className=''>
+                <div className='pe-3'>
                     <p> From </p>
                     <select className="form-select form-select-lg bg-dark text-white border-warning" aria-label=".form-select-lg example" value={from} onChange={(e) => setFrom(e.target.value)}>
                         <option>Select Currency </option>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='p-5 bg-dark text-warning border-top border-white'>
+    <div className='bg-dark text-warning border-top border-white' style={{height: "20vh"}}>
         <div className='d-flex justify-content-center p-4'>
             <h5> Group 5, PwC BootCamp  </h5>
         </div>
